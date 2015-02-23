b371d3eb7c3ecdf5275d0b4e3ecb5f7d6798dc51
Nuatu <nuatut@gmail.com>
2015-02-22 18:14:10 -0800

    Add function to render existing messages

--- src/clojure_guestbook/routes/home.clj
+++ src/clojure_guestbook/routes/home.clj
@@ -1,9 +1,29 @@
 (ns clojure-guestbook.routes.home
   (:require [compojure.core :refer :all]
-            [clojure-guestbook.views.layout :as layout]))
+            [clojure-guestbook.views.layout :as layout]
+            [hiccup.form :refer :all]))
+
+(defn show-guest []
+  [:ul.guests
+   (for [{:keys [message name timestamp]}
+         [{:message "Howdy" :name "Bob" :timestamp nil}
+          {message "Hi" :name "Bob" :timestamp nil}]]
+     [:li
+      [:blockquote message]
+      [:p "-" [:cite name]]
+      [:time timestammp]])])
+
 
 (defn home []
-  (layout/common [:h1 "Hello World!"]))
+  (layout/common
+   [:h1 "Guestbook"]
+   [:p "Leave a message"]
+   [:hr]
+   [:form
+     [:p "Name:"]
+     [:input]
+     [:p "Message:"]
+     [:textarea {:rows 10 :cols 40}]]))
 
 (defroutes home-routes
   (GET "/" [] (home)))
